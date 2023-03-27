a328b8c5f860f47a2ca510add44568ea7c5e57bf
wgaliano <wgaliano22@fed.idserve.net>
2023-03-27 17:31:15 +0200

    Initial Commit

---
+++ Breeds/BreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedsApp.swift
+//  Breeds
+//
+//  Created by Walter Galiano on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
